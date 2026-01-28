74a5ce9e9de0e8087b05185ed995d8b59c7add8d
Kadie Harrett <kadieharrett223@gmail.com>
2026-01-28 15:22:42 -0800

    feat: switch to Microsoft/Azure AD authentication

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,13 @@
 import type { NextAuthOptions } from "next-auth";
 import { getServerSession } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import AzureADProvider from "next-auth/providers/azure-ad";
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID || "",
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET || "",
+      tenantId: process.env.AZURE_AD_TENANT_ID || "",
     }),
   ],
   session: { strategy: "jwt" },
