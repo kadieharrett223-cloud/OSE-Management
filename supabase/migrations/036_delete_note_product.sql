e9eab1283fb23d8ce0cfec5eeeb7150d3f56d351
Kadie Harrett <kadieharrett223@gmail.com>
2026-02-17 12:02:28 -0800

    Revert note field changes - keeping note as product

--- supabase/migrations/036_delete_note_product.sql
+++
@@ -1,6 +0,0 @@
--- Delete 'note' product from price list
--- Moving 'note' functionality to a dedicated field on purchase_order_lines instead
-
-DELETE FROM price_list_items 
-WHERE LOWER(item_no) = LOWER('note') 
-OR LOWER(item_no) LIKE LOWER('%note%');
