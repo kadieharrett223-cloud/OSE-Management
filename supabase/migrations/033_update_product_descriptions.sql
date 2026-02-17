b06ea7f6b9a768259ede3f90ca2fdaefcfd67849
Kadie Harrett <kadieharrett223@gmail.com>
2026-02-17 10:49:25 -0800

    Add migration to update product descriptions to match PO specifications

---
+++ supabase/migrations/033_update_product_descriptions.sql
@@ -0,0 +1,55 @@
+-- Update product descriptions to match PO specifications
+
+UPDATE price_list_items SET description = 'Silver series 2 post base plate, 8,000 lb capacity, open carriage dual point lock release, secondary lock, 110" post, 2 stage arms, 2 stage adjustable foot, 3.5" truck adapter set, All pullies, cables, and hoses to be factory installed 110v 60hz 2.2kw, palletize power unit seperately' WHERE item_no = '2PBP-8';
+
+UPDATE price_list_items SET description = 'Silver series 2 post base plate, 8,000 lb capacity, open carriage dual point lock release, secondary lock, 110" post, 2 stage arms, 2 stage adjustable foot, 3.5" truck adapter set, All pullies, cables, and hoses to be factory installed 110v 60hz 2.2kw power unit, palletize power unit seperately' WHERE item_no = '2PBP-10';
+
+UPDATE price_list_items SET description = 'Gold series 2 post base plate, 10,000 lb capacity, open carriage single lock release, secondary lock, 110" H X 137" W pullies, cables, and hoses to be factory installed 3 stage arms, 3 stage adjustible foot, 3.5" truck adapter set 110v 60hz 2.2kw power unit, palletize power unit seperately' WHERE item_no = '2PBPXW-10';
+
+UPDATE price_list_items SET description = 'Gold series 2 post base plate, 12,000 lb capacity, 115" H X 153" W open carriage, single lock release, secondary lock, 115" x 156" pullies, cables, and hoses to be factory installed 3 stage arms, 3 stage adjustible foot, 3.5" truck adapter set 110v 60hz 2.2kw power unit, palletize power unit seperately' WHERE item_no = '2PBP-12';
+
+UPDATE price_list_items SET description = 'Silver Series 2 post clear floor, 9,000 lb capacity, 142" H X 129" W post, 2 stage arms, 2 stage foot, 2 lock release, open carriage, All pullies, cables, and hoses to be factory installed secondary lock, 3.5" truck adapter set, 110v 60hz 3kw Palletize power unit seperately' WHERE item_no = '2PCF-9';
+
+UPDATE price_list_items SET description = 'Gold Series 2 post clear floor, 10,000 lb capacity 153" H X 135" W chain drive, 3 stage arms, 3 stage foot, single lock release, secondary lock, pullies, cables, and hoses to be factory installed open carriage, 3.5" truck adapter set, 110v 60hz 2.2 kw Palletize power unit seperately' WHERE item_no = '2PCFXL-10';
+
+UPDATE price_list_items SET description = 'Gold Series 2 post clear floor symmetric / asymmetric post 10,000 lb capacity, 153" H X 145" W, direct drive, open carriage, 3 stage arms 3 stage foot, single lock release, scondary lock, 3.5" truck- pullies, cables, and hoses to be factory installed, 3" truck adapter set, 110v 60hz 2.2 kw, palletize power unit seperately' WHERE item_no = '2PDDA-10';
+
+UPDATE price_list_items SET description = 'Gold Series 2 post clear floor car lift, 12,000 lb capacity, 174" x 156" open carriage, 3 stage arms, 3 stage foot, single lock release, 3 stage foot, single lock release, scondary lock, 3.5" truck- pullies, cables, and hoses to be factory installed, 3" truck adapter set, 110v 60hz 2.2 kw, palletize power unit seperately' WHERE item_no = '2PCFHD-12';
+
+UPDATE price_list_items SET description = 'Gold Series 2 post clear floor car lift, 12,000 lb capacity, 174" x 156" open carriage, 3 stage arms, 3 stage foot, single lock release, 3 stage foot, single lock release, scondary lock, 3.5" truck- pullies, cables, and hoses to be factory installed, 3" truck adapter set, 110v 60hz 3kw, palletize power unit seperately' WHERE item_no = '2PCFHD-15';
+
+UPDATE price_list_items SET description = 'Gold Series 4-post car storage lift 9,000 lb. capacity 108" W X 198" L include tool tray, 3 drip trays, caster arms, drive through design, factory installed pullies, cables, and hoses 110v 60hz 2.2kw, palletize tool tray and power unit seperately' WHERE item_no = '4PML-9';
+
+UPDATE price_list_items SET description = 'Gold Series 4-post car storage lift 9,000 lb. capacity 136.9" W X 198.49" L sliding hitch rest, 3 drip trays, caster arms, adjustable width- runway, drive through design, factory installed pullies, cables, and hoses. 110v 60hz 2.2kw, palletize tool tray and power unit seperately' WHERE item_no = 'HDMBL-9';
+
+UPDATE price_list_items SET description = 'Gold Series 4 post High Rise Storage lift, 9,000 lb capacity, min 184" runways, min 87,67" drive through, min 78" storage space resting on top lock, tool tray, 3 drip trays, caster arms, pullies cables and hoses factory installed, 110v 60hz 2.2kw power palletize tool tray and power unit seperately' WHERE item_no = '4PHR-9X';
+
+UPDATE price_list_items SET description = 'Gold Series 4 post High Rise 2-car storage lift, portable, 10,000 lb capacity, 82" lifting height, 78" clearance resting on top lock, 208" W X 224" L, tool tray, 6 drip trays, caster arms, pullies, cables and hoses to be factory installed, 110v 60hz 2.2kw power units, palletize tool tray and power unit seperately' WHERE item_no = '4PXW-10';
+
+UPDATE price_list_items SET description = 'The Dually Gold Series 4 post High Rise Storage lift, portable, 10,000 lb capacity, min 84" Storage on top lock, 112" drive through, 195" runways, tool tray, 4 drip trays, caster arms , 110v 60Hz 2.2kw - power units, Pullies, cables, and hoses factory installed, palletize tool tray and power unit seperately' WHERE item_no = '4PXL-10';
+
+UPDATE price_list_items SET description = 'Gold Series 3-car stacking lift, 16,000 lb capacity, column height 174", overall width 113.7", overall length 229.4", lower platform 9,000 lb capacity, upper platform 7,000 lb capacity. Lower platform 82" rise, max lifting height 87.2" runway length 185.35", between the platforms 100". Upper platform max rise 151.4", runway length 199", max lifting height 156.5", distance between the columns 100". concrete anchors, Include concrete anchors, eight drip trays, steel ramps, Pullies, cables, and hoses factory installed, 220V 3KW 60 Hz PU. Include the power unit in the package' WHERE item_no = '4032XL';
+
+UPDATE price_list_items SET description = 'Gold Series 4 post maintenance lift, 12,000 lb capacity, overall height 87.40", width, 132.40", length 245.47", runway length 196.85", between the posts 114.37", lifting height 70.86", runway width 21.6". Manual lock. Include tool tray, 3 drip trays, 220v 60hz 2.2kw power unit. Pullies, cables, and hoses factory installed, 220V 2.2KW 60 Hz PU. Palletize tool tray and power unit seperately.' WHERE item_no = '4PHDXL-12';
+
+UPDATE price_list_items SET description = 'Gold Series 4 post alignment lift, 14,000 lb capacity, overall height 87.40", width 99.60", length 246.06", between the posts 116.65", drive through 99.60", runway width 21.65", runway length 196.85", air lock release, slip plates, turntables, tool tray. two air/hyddraulic bridge jack (packaged seperately), 220v 60hz 3kw power unit. Pullies, cables, and hoses factory installed. Please pack power unit with package' WHERE item_no = '4PHDXLA-14';
+
+UPDATE price_list_items SET description = 'Gold Series 4 post open front alignment lift, 11,000 lb capacity, overall height 87.40", width 137.8", length 246.06", drive through 99.60" lifting height 70.86" runway length 196.85" runway width 21.65". air lock release, slip plates, turntables, two air/hyddraulic bridge jacks (packaged seperately) 220v 60Hz 2.2KW power unit, pullies- cables, and hoses factory installed. Please pack power unit and accessories with package' WHERE item_no = '4PHDXLA-11';
+
+UPDATE price_list_items SET description = 'Gold Series 4 post open front alignment lift, 15,000 lb capacity, overall height 87.40", width 137.8", length 246.06", drive through 99.60" lifting height 70.86" runway length 196.85" runway width 21.65". air lock release, slip plates, turntables, two air/hyddraulic bridge- jacks (packaged seperately) 220v 60Hz 3KW, power units, pullies- cables, and hoses factory installed. Please pack power unit and accessories with package' WHERE item_no = '4PHDXLA-15';
+
+UPDATE price_list_items SET description = 'Ultra-thin Portable Full-Rise Scissor Lift 73" rise 7800 lb capacity Overall width 81.3", length 80", minimum height 4.1", maximum lift height 72.8", 24V safety voltage control system, pneumatic release, 220v 2.2kw 60hz power unit' WHERE item_no = 'FRSL-78';
+
+UPDATE price_list_items SET description = 'Portable Mid-rise scissor lift 30" rise, 7500 lb capacity, maximum height 39", minimum height 3.9", overall width 69.2", length 79.2" table width 33", mechanical lock, dual cylinders, pneumatic/ electric pump release, portable power cart, 110v 2.2kw 60hz power unit' WHERE item_no = 'MRSL-75';
+
+UPDATE price_list_items SET description = 'Portable Mid-Rise Scissor Lift, 6,000 capacity, overall width 72.3", min height 3.9", max lifting height 48.22", table length 70", table width 42.5", includes truck adapters, 2.2KW, 60Hz, power unit.' WHERE item_no = 'MRSL-6';
+
+UPDATE price_list_items SET description = '220v 2.2kw 60hz power unit' WHERE item_no = 'HPU220';
+
+UPDATE price_list_items SET description = '220v 3kw 60hz power unit' WHERE item_no = 'HPU4';
+
+UPDATE price_list_items SET description = '6" truck adapters, per piece' WHERE item_no = '4PTA6';
+
+UPDATE price_list_items SET description = '6,000 lb hand pump rolling bridge Jack' WHERE item_no = 'HLCJ-6';
+
+UPDATE price_list_items SET description = 'Air / hydraulic rolling bridge Jack for 14K alignment lift' WHERE item_no = 'HLCJ-14';
