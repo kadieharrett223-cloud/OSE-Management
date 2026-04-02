6831e9b5552e0bfa4b30e743c9f61afad7e7be18
Kadie Harrett <kadieharrett223@gmail.com>
2026-04-02 08:31:10 -0700

    update FOB costs for 15 products

---
+++ supabase/migrations/063_update_fob_costs.sql
@@ -0,0 +1,18 @@
+-- Update FOB costs per supplier price list update
+UPDATE price_list_items SET fob_cost = 818    WHERE item_no = '2PBP-8';
+UPDATE price_list_items SET fob_cost = 868    WHERE item_no = '2PBP-10';
+UPDATE price_list_items SET fob_cost = 938    WHERE item_no = '2PBPXW-10';
+UPDATE price_list_items SET fob_cost = 1230   WHERE item_no = '2PBP-12';
+UPDATE price_list_items SET fob_cost = 898    WHERE item_no = '2PCF-9';
+UPDATE price_list_items SET fob_cost = 1028   WHERE item_no = '2PCFXL-10';
+UPDATE price_list_items SET fob_cost = 1158   WHERE item_no = '2PDDA-10';
+UPDATE price_list_items SET fob_cost = 1338   WHERE item_no = '2PCFHD-12';
+UPDATE price_list_items SET fob_cost = 1965   WHERE item_no = '2PCFHD-15';
+UPDATE price_list_items SET fob_cost = 1230   WHERE item_no = '4PML-9';
+UPDATE price_list_items SET fob_cost = 1295   WHERE item_no = 'HDMBL-9';
+UPDATE price_list_items SET fob_cost = 2015   WHERE item_no = '4PHDXL-12';
+-- NOTE: 4PHDXLA-12 not found in DB; nearest variants are 4PHDXLA-11 and 4PHDXLA-14
+-- UPDATE price_list_items SET fob_cost = 2140   WHERE item_no = '4PHDXLA-12';
+UPDATE price_list_items SET fob_cost = 780    WHERE item_no = 'MRSL-6';
+UPDATE price_list_items SET fob_cost = 860    WHERE item_no = 'MRSL-75';
+UPDATE price_list_items SET fob_cost = 1500   WHERE item_no = 'FRSL-78';
